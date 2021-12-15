b2287a2fb1042b50b6b2d20988f2d81096f9a285
Brandon H. Goding <brandon.h.goding@gmail.com>
2021-12-14 22:18:09 -0500

    Empty Membership models to test Black and Flake8 Precommit Hooks

--- membership/models.py
+++ membership/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class MembershipType(models.Model):
+    pass
+
+
+class Membership(models.Model):
+    pass
+
+
+class Member(models.Model):
+    pass
+
