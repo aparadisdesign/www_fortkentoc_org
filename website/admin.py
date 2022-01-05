7c5482dc1ce34e72434e640d68c2ae2b29904bef
Brandon H. Goding <brandon.h.goding@gmail.com>
2022-01-04 21:36:12 -0500

    Working on boardmembers and sponsor models

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+import website.models as website_models
 
-# Register your models here.
+
+@admin.register(website_models.BoardMember)
+class BoardMemberAdmin(admin.ModelAdmin):
+    list_display = ['last_name', 'first_name', 'office_held']
+    list_display_links = ['last_name', 'first_name']
+    list_filter = ['last_name', 'first_name']
+    ordering = ['last_name', 'first_name']
+
+
+@admin.register(website_models.Sponsor)
+class SponsorAdmin(admin.ModelAdmin):
+    list_display = ['name', 'level', 'start_date', 'end_date']
+    list_filter = ['name', 'level', 'start_date', 'end_date']
+    ordering = ['start_date', 'name']
--- website/models.py
+++ website/models.py
@@ -52,3 +52,31 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.email
+
+
+class BoardMember(models.Model):
+    first_name = models.CharField(max_length=65, null=False, blank=False)
+    last_name = models.CharField(max_length=65, null=False, blank=False)
+    office_held = models.CharField(max_length=99, null=True, blank=True)
+    image = models.ImageField(upload_to='uploads/board_members/% Y/% m/% d/', null=True, blank=True)
+
+    def __str__(self):
+        if self.office_held:
+            return f"{self.last_name}, {self.first_name} - {self.office_held}"
+        return f"{self.last_name}, {self.first_name}"
+
+    class Meta:
+        ordering = ['last_name', 'first_name']
+
+
+class Sponsor(models.Model):
+    name = models.CharField(max_length=65, null=False, blank=False)
+    level = models.CharField(max_length=15, null=True, blank=True)
+    start_date = models.DateField(null=True, blank=True)
+    end_date = models.DateField(null=True, blank=True)
+    image = models.ImageField(upload_to='uploads/sponsors/% Y/% m/% d/', null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
